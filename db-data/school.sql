6320c89c723e6707a1075999fb900f7a64ac6400
Achyuth-R <shadyachyuth@gmail.com>
2017-09-05 18:19:52 +0530

    add initial database

---
+++ db-data/school.sql
@@ -0,0 +1,18 @@
+create table student {
+    register_id number(9),
+    f_name varchar2(10),
+    l_name varchar2(10)
+};
+
+create table class {
+    class_number varchar2(3),
+    class_teacher varchar2(15),
+    class_strength 
+};
+
+
+create table teacher{
+    emp_id number(5),
+    f_name varchar2(10),
+    l_name varchar2(10)
+};
