66d2aefbdb1c0eb8635cbacdbad79740dd68ab7f
Achyuth-R <shadyachyuth@gmail.com>
2017-09-07 09:07:58 +0530

    file : school.sql, syntax errors

--- db-data/school.sql
+++ db-data/school.sql
@@ -1,45 +1,45 @@
-create table student {
+create table student (
     register_id     number(9),
     f_name          varchar(10),
     l_name          varchar(10)
-};
+);
 
-create table class {
+create table class (
     class_number    varchar(3),
     class_teacher   varchar(15),
     class_strength  number(2)
-};
+);
 
 
-create table teacher{
+create table teacher(
     emp_id          number(5),
     f_name          varchar(10),
     l_name          varchar(10)
-};
+);
 
-create table personal_info {
+create table personal_info (
     gender          varchar(6),
     blood_group     varchar(2),
     date_of_birth   date,
     Address         text,
     E-mail          text,
     contact         text
-};
+);
 
-create table guardian {
+create table guardian (
     father_name     varchar(15),
     mother_name     varchar(15),
     guardian_name   varchar(15),
     password        varchar(10)        
-};
+);
 
-create table attendance{
+create table attendance(
     days_attended   number(3),
     days_missed     number(3),
     attendance      number(2)
-};
+);
 
-create table marks{
+create table marks(
     quarterly       number(3),
     halfyearly      number(3),
     annual          number(3),
@@ -49,6 +49,6 @@ create table marks{
     punctuality     number(3),
     behaviour       number(3),
     enthusiasm      number(3)
-};
+);
 
 
