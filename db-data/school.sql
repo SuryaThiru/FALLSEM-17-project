529d3cde37ba5332abd65eba05c84e31f4cf4f68
Achyuth-R <shadyachyuth@gmail.com>
2017-09-07 09:36:53 +0530

    file replace : school.sql mixup in file, this iteration contains relationships and no syntax errors

--- db-data/school.sql
+++ db-data/school.sql
@@ -1,45 +1,51 @@
 create table student (
-    register_id     number(9),
-    f_name          varchar(10),
-    l_name          varchar(10)
+    register_id     number(4), --primary key
+    f_name          text,
+    l_name          text,
+    class_number    varchar(3) --foreign key
 );
 
 create table class (
-    class_number    varchar(3),
-    class_teacher   varchar(15),
+    class_number    varchar(3), --primary key
+    class_teacher   number(5),
     class_strength  number(2)
 );
 
 
 create table teacher(
-    emp_id          number(5),
+    emp_id          number(5), --primary key
     f_name          varchar(10),
-    l_name          varchar(10)
+    l_name          varchar(10),
+    subject         text,
+    password        text
 );
 
 create table personal_info (
-    gender          varchar(6),
+    register_id     number(4), --foreign key
+    gender          char(1),
     blood_group     varchar(2),
     date_of_birth   date,
     Address         text,
     E-mail          text,
-    contact         text
+    contact         number(10)
 );
 
 create table guardian (
+    register_id     number(4), --foreign key
     father_name     varchar(15),
     mother_name     varchar(15),
     guardian_name   varchar(15),
-    password        varchar(10)        
+    password        text(10)        
 );
 
-create table attendance(
-    days_attended   number(3),
-    days_missed     number(3),
-    attendance      number(2)
+create table attendence (
+    class_date     date,    
+    register_id    number(4),   --foreign key
+    attendence     bool
 );
 
 create table marks(
+    register_id     number(4), --foreign key
     quarterly       number(3),
     halfyearly      number(3),
     annual          number(3),
@@ -51,4 +57,9 @@ create table marks(
     enthusiasm      number(3)
 );
 
-
+create table taught_by (
+    class_number    number(3), 
+    emp_id          number(5),
+    class_timing    time,
+    class_code      varchar(6)
+);
